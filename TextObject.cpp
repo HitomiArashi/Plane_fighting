e24faa20d146d5194cc8d6bf87f44cb294c72598
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 19:39:03 +0700

    Update TextObject.cpp

--- TextObject.cpp
+++ TextObject.cpp
@@ -1,18 +1,12 @@
 #include "TextObject.h"
 
-using namespace std;
-
 TextObject::TextObject()
 {
 	//Todo
 	rect_.x = 500;
-	
 	rect_.y = 10;
-
 	text_color_.r = 0;
-
 	text_color_.g = 0;
-
 	text_color_.b = 0;
 }
 
@@ -29,13 +23,11 @@ void TextObject::SetColor(const int& type)
 		SDL_Color color = { 255, 0, 0 };
 		text_color_ = color;
 	}
-	
 	else if (type == WHITE_TEXT)
 	{
 		SDL_Color color = { 255, 255, 255 };
 		text_color_ = color;
 	}
-
 	else
 	{
 		SDL_Color color = { 0, 0, 0 };
@@ -46,8 +38,5 @@ void TextObject::SetColor(const int& type)
 void TextObject::CreateGameText(TTF_Font* font, SDL_Surface* des)
 {
 	p_object_ = TTF_RenderText_Solid(font, str_val_.c_str(), text_color_);
-	
 	Show(des);
-
-
 }
