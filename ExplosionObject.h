b76fb0a6f71132384a75c816cebfd0034f50a771
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 19:38:48 +0700

    Update ExplosionObject.h

--- ExplosionObject.h
+++ ExplosionObject.h
@@ -5,7 +5,6 @@
 #define EXPLOSION_H_
 
 #include "BulletObject.h"
-
 #include "Common_Function.h"
 
 const int EXP_WIDTH = 165;
@@ -17,22 +16,17 @@ class Explosion : public BaseObject
 public:
 
 	Explosion();
-
 	~Explosion();
-
 	void set_clip();
-
 	void set_frame(const int& fr)
 	{
 		frame_ = fr;
 	}
-
 	void ShowEx(SDL_Surface* des);
 
 private:
 
 	int frame_;
-
 	SDL_Rect clip_[4];
 };
 #endif
