00a16b4183b373988ea176875abf32bef79cc580
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 21:33:15 +0700

    Update ExplosionObject.cpp

--- ExplosionObject.cpp
+++ ExplosionObject.cpp
@@ -1,3 +1,5 @@
+//The header and library will use
+
 #include "ExplosionObject.h"
 
 using namespace SDLCommonFunc;
@@ -12,29 +14,36 @@ Explosion::~Explosion()
 	//Todo
 	;
 }
+
+//Set info for each esplosion frame
+
 void Explosion::set_clip()
 {
-	//Todo
 	clip_[0].x = 0;
 	clip_[0].y = 0;
 	clip_[0].w = EXP_WIDTH;
 	clip_[0].h = EXP_HEIGHT;
+
 	clip_[1].x = EXP_WIDTH;
 	clip_[1].y = 0;
 	clip_[1].w = EXP_WIDTH;
 	clip_[1].h = EXP_HEIGHT;
+
 	clip_[2].x = EXP_WIDTH * 2;
 	clip_[2].y = 0;
 	clip_[2].w = EXP_WIDTH;
 	clip_[2].h = EXP_HEIGHT;
+
 	clip_[3].x = EXP_WIDTH * 3;
 	clip_[3].y = 0;
 	clip_[3].w = EXP_WIDTH;
 	clip_[3].h = EXP_HEIGHT;
 }
+
+//Show the explosion when collid
+
 void Explosion::ShowEx(SDL_Surface* des)
-{
-	//Todo
+{	
 	if (frame_ >= 4)
 	{
 		frame_ = 0;
