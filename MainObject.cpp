0d6904308403fddf0480fab41ecba7bdd5004155
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 19:38:53 +0700

    Update MainObject.cpp

--- MainObject.cpp
+++ MainObject.cpp
@@ -1,22 +1,16 @@
 //The header and library will use
 
 #include "MainObject.h"
-using namespace std;
 
 //Initialize the property of the main object
 
 MainObject::MainObject()
 {
 	rect_.x = 0;
-
 	rect_.y = 0;
-
 	rect_.w = WIDTH_MAIN_OBJECT;
-
 	rect_.h = HEIGHT_MAIN_OBJECT;
-
 	x_val_ = 0;
-
 	y_val_ = 0;
 }
 
@@ -45,18 +39,14 @@ void MainObject::MakeBullet(SDL_Surface* g_scren)
 			if (p_bullet->get_is_move())
 			{
 				p_bullet->Show(g_scren);
-
 				p_bullet->HandleMove(SCREEN_WIDTH, SCREEN_HEIGHT);
 			}
-
 			else
 			{
 				if (p_bullet != NULL)
 				{
 					p_bullet_list_.erase(p_bullet_list_.begin());
-
 					delete p_bullet;
-
 					p_bullet = NULL;
 				}
 			}
@@ -75,105 +65,58 @@ void MainObject::HandleInputAction(SDL_Event events, Mix_Chunk* bullet_sound[2])
 		switch (events.key.keysym.sym)
 		{
 		case SDLK_UP: //Button up arrow
-
 			y_val_ -= HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_w: //Button "w"
-
 			y_val_ -= HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_DOWN: //Button down arrow
-
 			y_val_ += HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_s: //Button "s"
-
 			y_val_ += HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_LEFT: //Button left arrow
-
 			x_val_ -= WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_a: //Button "a"
-
 			x_val_ -= WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_RIGHT: //Button right arrow
-
 			x_val_ += WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_d: //Button "d"
-
 			x_val_ += WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
 		}
 	}
-
 	else if (events.type == SDL_KEYUP) //Stop moving
 	{
 		switch (events.key.keysym.sym)
 		{
 		case SDLK_UP: //Button up arrow
-
 			y_val_ += HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_w: //Button "w"
-
 			y_val_ += HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_DOWN: //Button down arrow
-
 			y_val_ -= HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_s: //Button "s"
-
 			y_val_ -= HEIGHT_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_LEFT: //Button left arrow
-
 			x_val_ += WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_a: //Button "a"
-
 			x_val_ += WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_RIGHT: //Button right arrow
-
 			x_val_ -= WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
-
 		case SDLK_d: //Button "d"
-
 			x_val_ -= WIDTH_MAIN_OBJECT / MAIN_OBJECT_SPEED;
-
 			break;
 		}
 	}
@@ -189,35 +132,26 @@ void MainObject::HandleInputAction(SDL_Event events, Mix_Chunk* bullet_sound[2])
 		if (events.button.button == SDL_BUTTON_LEFT)
 		{
 			p_bullet->SetWidthHeight(WIDTH_LASER, HEIGHT_LASER);
-
 			p_bullet->LoadImg(MAIN_BULLET_LASER);
-
 			p_bullet->set_type(BulletObject::LASER);
-
 			Mix_PlayChannel(-1, bullet_sound[0], 0);
 		}
 
-		//Set the bullet is bullet
+		//Set the bullet is sphere
 
 		else if (events.button.button == SDL_BUTTON_RIGHT)
 		{
 			p_bullet->SetWidthHeight(WIDTH_SPHERE, HEIGHT_SPHERE);
-
 			p_bullet->LoadImg(MAIN_BULLET_SPHERE);
-
 			p_bullet->set_type(BulletObject::SPHERE);
-
 			Mix_PlayChannel(-1, bullet_sound[1], 0);
 		}
 
 		//Show the bullet
 
 		p_bullet->SetRect(this->rect_.x + this->rect_.w - 20, this->rect_.y + this->rect_.h * 0.73);
-
 		p_bullet->set_is_move(true);
-
 		p_bullet->set_x_val(MAIN_BULLET_SPEED);
-
 		p_bullet_list_.push_back(p_bullet);
 	}
 
@@ -237,19 +171,18 @@ void MainObject::HandleInputAction(SDL_Event events, Mix_Chunk* bullet_sound[2])
 void MainObject::HandleMove()
 {
 	rect_.x += x_val_;
-
 	if (rect_.x < 0 || rect_.x + WIDTH_MAIN_OBJECT > SCREEN_WIDTH)
 	{
 		rect_.x -= x_val_;
 	}
-
 	rect_.y += y_val_;
-
 	if (rect_.y < 0 || rect_.y + HEIGHT_MAIN_OBJECT > SCREEN_HEIGHT - 0)
 	{
 		rect_.y -= y_val_;
 	}
 }
+
+//Remove the bullet
 
 void MainObject::RemoveBullet(const int& idx)
 {
@@ -258,13 +191,10 @@ void MainObject::RemoveBullet(const int& idx)
 		if (idx < p_bullet_list_.size())
 		{
 			BulletObject* p_bullet = p_bullet_list_.at(idx);
-
 			p_bullet_list_.erase(p_bullet_list_.begin() + idx);
-
 			if (p_bullet != NULL)
 			{
 				delete p_bullet;
-
 				p_bullet = NULL;
 			}
 		}
