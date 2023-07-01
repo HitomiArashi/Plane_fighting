e7a10afa70448f4f3a43a8d61f11a5b7d126b8d6
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 19:39:09 +0700

    Update ThreatsObject.cpp

--- ThreatsObject.cpp
+++ ThreatsObject.cpp
@@ -2,22 +2,15 @@
 
 #include "ThreatsObject.h"
 
-using namespace std;
-
 //Initialize the property of the threat object
 
 ThreatObject::ThreatObject()
 {
 	rect_.x = SCREEN_WIDTH;
-
 	rect_.y = SCREEN_HEIGHT * 0.5;
-
 	rect_.w = WIDTH_THREAT;
-
 	rect_.h = HEIGHT_THREAT;
-
 	x_val_ = 0;
-
 	y_val_ = 0;
 }
 
@@ -34,7 +27,6 @@ ThreatObject::~ThreatObject()
 			if (p_bullet != NULL)
 			{
 				delete p_bullet;
-
 				p_bullet = NULL;
 			}
 		}
@@ -49,19 +41,13 @@ void ThreatObject::InitBullet(BulletObject* p_bullet)
 	if (p_bullet)
 	{
 		bool ret = p_bullet->LoadImg(THREAT_BULLET);
-
 		if (ret)
 		{
 			p_bullet->set_is_move(true);
-
 			p_bullet->SetWidthHeight(WIDTH_SPHERE, HEIGHT_SPHERE);
-
 			p_bullet->set_type(BulletObject::SPHERE);
-
 			p_bullet->SetRect(rect_.x, rect_.y + rect_.h * 0.5);
-
 			p_bullet->set_x_val(THREAT_BULLET_SPEED);
-
 			p_bullet_list_.push_back(p_bullet);
 		}
 	}
@@ -74,20 +60,16 @@ void ThreatObject::MakeBullet(SDL_Surface* des, const int& x_limit, const int& y
 	for (int i = 0; i < p_bullet_list_.size(); i++)
 	{
 		BulletObject* p_bullet = p_bullet_list_.at(i);
-
 		if (p_bullet)
 		{
 			if (p_bullet->get_is_move())
 			{
 				p_bullet->Show(des);
-
 				p_bullet->HandleMoveRightToLeft();
 			}
-
 			else
 			{
 				p_bullet->set_is_move(true);
-
 				p_bullet->SetRect(rect_.x, rect_.y + rect_.h * 0.5);
 			}
 		}
@@ -107,12 +89,10 @@ void ThreatObject::HandleMove(const int& x_bordr, const int& y_bordr)
 		//Set the random spawn for the threat
 
 		int rand_y = rand() % VAL_OFFSET_START_POS_THREATS;
-
 		if (rand_y > SCREEN_HEIGHT - UNDER_LIMIT_THREAT)
 		{
 			rand_y = SCREEN_HEIGHT * 0.3;
 		}
-
 		rect_.y = rand_y;
 	}
 }
@@ -132,14 +112,11 @@ void ThreatObject::Reset(const int& xborder)
 	//Set the random spawn for the threat
 
 	int rand_y = rand() % VAL_OFFSET_START_POS_THREATS;
-
 	if (rand_y > SCREEN_HEIGHT - UNDER_LIMIT_THREAT)
 	{
 		rand_y = SCREEN_HEIGHT * 0.3;
 	}
-
 	rect_.y = rand_y;
-
 	for (int i = 0; i < p_bullet_list_.size(); i++)
 	{
 		BulletObject* p_bullet = p_bullet_list_.at(i);
