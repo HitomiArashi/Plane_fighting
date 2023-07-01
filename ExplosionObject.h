025dbeb0dd795f98c60a873cd484ac98e4bf191f
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 21:33:17 +0700

    Update ExplosionObject.h

--- ExplosionObject.h
+++ ExplosionObject.h
@@ -4,8 +4,12 @@
 
 #define EXPLOSION_H_
 
+//The header and library will use
+
 #include "BulletObject.h"
 #include "Common_Function.h"
+
+//Info of each frame
 
 const int EXP_WIDTH = 165;
 
@@ -17,11 +21,20 @@ public:
 
 	Explosion();
 	~Explosion();
+
+	//Set the clip of explosion will be shown
+
 	void set_clip();
+
+	//Set the frame for the clip
+
 	void set_frame(const int& fr)
 	{
 		frame_ = fr;
 	}
+
+	//Show the clip of explosion
+
 	void ShowEx(SDL_Surface* des);
 
 private:
