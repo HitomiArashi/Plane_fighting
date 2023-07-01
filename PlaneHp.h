4c3f9c62006f8113963d386329d4a90ca8817d0b
HitomiArashi <manhhunghlqn69@gmail.com>
2023-07-01 19:39:01 +0700

    Update PlaneHp.h

--- PlaneHp.h
+++ PlaneHp.h
@@ -5,37 +5,27 @@
 #define PLANE_HP_H_
 
 #include "BaseObject.h"
-
 #include <vector>
-
-using namespace std;
 
 class PlaneHp : public BaseObject
 {
 public:
 
 	PlaneHp();
-
 	~PlaneHp();
-
 	void SetNumber(const int& num)
 	{
 		number_ = num;
 	}
-
 	void Init();
-	
 	void AddPos(const int& xpos);
-
 	void Render(SDL_Surface* des);
-
 	void Decrease();
 
 private:
 
 	int number_;
-
-	vector <int>  pos_list_;
+	std::vector <int>  pos_list_;
 };
 
 #endif // !PLANE_HP_H_
